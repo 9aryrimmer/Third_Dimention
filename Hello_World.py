020dfa65f1e7585e43d9e9c37b8e88570ccd2325
Gary Rimmer <126683944+9aryrimmer@users.noreply.github.com>
2023-03-02 20:50:21 +0000

    Add files via upload

---
+++ Hello_World.py
@@ -0,0 +1,63 @@
+
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.clock import Clock
+from kivy.properties import NumericProperty
+from kivy.uix.floatlayout import FloatLayout
+
+from kivy.uix.button import Button
+
+
+Window.clearcolor=(0,0,0,1)
+
+class FloatLayout(FloatLayout):
+    Window.size=(500,500)
+    ball_x=NumericProperty(.01)
+    ball_y=NumericProperty(.5)
+    
+    
+    pluss_minus_x=.01
+    pluss_minus_y=.01
+   
+   
+
+
+    def __init__(self,**kwargs):
+        super(FloatLayout,self).__init__(**kwargs)
+
+        Clock.schedule_interval(self.ball_bounce,1/60)
+
+        return
+
+    def ball_bounce(self,dt):
+
+        self.ball_x+=self.pluss_minus_x
+        self.ball_y+=self.pluss_minus_y
+        
+        self.ball_y+=.001
+
+        if self.ball_x<.01:
+            self.pluss_minus_x=.01
+
+        if self.ball_x>.99:
+            self.pluss_minus_x=-.01
+
+        if self.ball_y>.99:
+            self.pluss_minus_y=-.01
+
+        if self.ball_y<.01:
+            self.pluss_minus_y=.01
+
+        
+        return
+
+    
+
+class MyApp(App):
+    pass
+    def build(self):
+        
+        sm=root=FloatLayout()
+        return sm
+
+MyApp().run() 
