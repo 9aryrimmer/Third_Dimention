8dcdda03ead78058c8dc3774c9078ff0ca792175
Gary Rimmer <126683944+9aryrimmer@users.noreply.github.com>
2023-03-03 16:23:10 +0000

    Add files via upload

--- Ball.py
+++ Ball.py
@@ -2,7 +2,7 @@
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.clock import Clock
-from kivy.properties import NumericProperty
+from kivy.properties import NumericProperty, ObjectProperty
 from kivy.uix.floatlayout import FloatLayout
 
 from kivy.uix.button import Button
@@ -14,7 +14,7 @@ class FloatLayout(FloatLayout):
     Window.size=(500,500)
     ball_x=NumericProperty(.01)
     ball_y=NumericProperty(.5)
-    
+    pressed=ObjectProperty(False)
     
     pluss_minus_x=.01
     pluss_minus_y=.01
@@ -45,9 +45,9 @@ class FloatLayout(FloatLayout):
         if self.ball_y>.99:
             self.pluss_minus_y=-.01
 
-        if self.ball_y<.01:
+        if self.ball_y<.01 or self.pressed==True:
             self.pluss_minus_y=.01
-
+            self.pressed=False
         
         return
 
